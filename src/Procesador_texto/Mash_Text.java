7ab9ec34eba06ec234c429c03bfb3577f2bea91f
moi <moisessepseg@gmail.com>
2021-06-01 12:57:50 +0200

    Editor de texto con mas opciones
    He conseguido ahcer el editos y agregarle opciones como guardar, abrir ect...

--- src/Procesador_texto/Mash_Text.java
+++ src/Procesador_texto/Mash_Text.java
@@ -2,15 +2,30 @@ package Procesador_texto;
 
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 
 public class Mash_Text  extends JFrame{
 	
+	private JMenuItem nuevo;
+	private JMenuItem guardar;
+	private JMenuItem abrir;
+	private JMenuItem salir;
+	private JMenu files;
+	private JMenuBar menup;
 	private JTextArea mostrar;
 
 	public Mash_Text () {
@@ -24,12 +39,19 @@ public class Mash_Text  extends JFrame{
 		Font letra1 = new Font("Arial", Font.BOLD,  20);
 				
 		//Mostrar
-		mostrar = new JTextArea ("ESTO ES UNA PRUEBA");
+		mostrar = new JTextArea (" ");
 		mostrar.setBounds(50,55,700,500);
 
+		//Menu
+		menup = new JMenuBar ();
+		setJMenuBar(menup);
 		
-		//Botones
-		
+		//Barra File
+		files = new JMenu ("File");
+		nuevo = new JMenuItem ("Nuevo");
+		abrir = new JMenuItem ("Abrir");
+		guardar = new JMenuItem ("Guardar");
+		salir = new JMenuItem ("Salir");
 
 		
 		//JPanel
@@ -44,7 +66,97 @@ public class Mash_Text  extends JFrame{
 		//Aniadir cosas
 		contenedor.add(principal);
 		principal.add(mostrar);
+		principal.add(menup);
+		
+		//Aniadir menu barra
+		menup.add(files); 
+		
+		//Aniadir menu 
+		files.add(nuevo);
+		files.add(guardar);
+		files.add(abrir);
+		files.add(salir);
+		
+		//Eventos
+		//Salir
+		salir.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	                System.exit(0);
+	        }
+	    });
+		
+		//Guardar
+		guardar.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	javax.swing.JFileChooser jF1= new javax.swing.JFileChooser();
+	            String ruta = "";
+	             try{
+	                if(jF1.showSaveDialog(null)==jF1.APPROVE_OPTION){
+	                  ruta = jF1.getSelectedFile().getAbsolutePath();
+	                  FileWriter archivo = new FileWriter (ruta);
+	                  archivo.write(mostrar.getText());
+	                  archivo.close();
+	                  
+	                //Aqui ya tiens la ruta,,,ahora puedes crear un fichero n esa ruta y escribir lo k kieras...
+	                }
+	              }catch (Exception ex){
+	                ex.printStackTrace();
+	              }
+	        }
+	    });
+		
+		//Abrir
+		abrir.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	mostrar.setText(abrirArchivo());
+	        }
+	    });
+		
+		//Nuevo
+		nuevo.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	mostrar.setText("");
+	        }
+	    });
+	//Fin constructor	
 	}
+	
+	
+	//Funcion para abrir archivos
+	private String abrirArchivo() {
+		  String aux="";   
+		  String texto="";
+		  try
+		  {
+		   /**llamamos el metodo que permite cargar la ventana*/
+		   JFileChooser file=new JFileChooser();
+		   file.showOpenDialog(this);
+		   /**abrimos el archivo seleccionado*/
+		   File abre=file.getSelectedFile();
+
+		   /**recorremos el archivo, lo leemos para plasmarlo
+		   *en el area de texto*/
+		   if(abre!=null)
+		   {     
+		      FileReader archivos=new FileReader(abre);
+		      BufferedReader lee=new BufferedReader(archivos);
+		      while((aux=lee.readLine())!=null)
+		      {
+		         texto+= aux+ "\n";
+		      }
+		         lee.close();
+		    }    
+		   }
+		   catch(IOException ex)
+		   {
+		     JOptionPane.showMessageDialog(null,ex+"" +
+		           "\nNo se ha encontrado el archivo",
+		                 "ADVERTENCIA!!!",JOptionPane.WARNING_MESSAGE);
+		    }
+		  return texto;//El texto se almacena en el JTextArea
+		}
+
+
 		
 	
 	
