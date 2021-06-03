5d22443d4e3eb9b44a826c6f7adc1dc432ef29d3
moi <moisessepseg@gmail.com>
2021-06-03 09:59:05 +0200

    Mash_text areglado
    Ya solo queda poner las opciones para ajustar la fuente y para cambiar el tipo de letra

--- src/Procesador_texto/Mash_Text.java
+++ src/Procesador_texto/Mash_Text.java
@@ -8,15 +8,23 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.font.TextAttribute;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.text.DefaultEditorKit;
+import javax.swing.text.EditorKit;
+import javax.swing.text.StyledEditorKit;
+
+import org.w3c.dom.Text;
 
 public class Mash_Text  extends JFrame{
 	
@@ -27,31 +35,88 @@ public class Mash_Text  extends JFrame{
 	private JMenu files;
 	private JMenuBar menup;
 	private JTextArea mostrar;
+	
+	//Botones
+	private JButton negrita;
+	private JButton cursiva;
+	private JButton subr;
 
 	public Mash_Text () {
 		setTitle("Mash_Text");
-		setSize(800,600);
+		setSize(800,1000);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		//Tipos de fuentes
-		Font letra1 = new Font("Arial", Font.BOLD,  20);
+		Font letra1 = new Font("Arial", Font.BOLD,  12);
+		Font letra2 = new Font("Arial", Font.ITALIC,  12);
+		Font letra3 = new Font("Arial", TextAttribute.UNDERLINE_ON, 12);
 				
 		//Mostrar
 		mostrar = new JTextArea (" ");
-		mostrar.setBounds(50,55,700,500);
+		mostrar.setBounds(0,60,800,1000);
 
 		//Menu
 		menup = new JMenuBar ();
 		setJMenuBar(menup);
 		
 		//Barra File
-		files = new JMenu ("File");
+		files = new JMenu ("Archivo");
 		nuevo = new JMenuItem ("Nuevo");
 		abrir = new JMenuItem ("Abrir");
 		guardar = new JMenuItem ("Guardar");
 		salir = new JMenuItem ("Salir");
+		
+		//Barra editar
+		  JMenu editMenu = new JMenu("Editar");
+
+	      Action cutAction = new DefaultEditorKit.CutAction();
+	      cutAction.putValue(Action.NAME, "Cortar");
+	      editMenu.add(cutAction);
+
+	      Action copyAction = new DefaultEditorKit.CopyAction();
+	      copyAction.putValue(Action.NAME, "Copiar");
+	      editMenu.add(copyAction);
+
+	      Action pasteAction = new DefaultEditorKit.PasteAction();
+	      pasteAction.putValue(Action.NAME, "Pegar");
+	      editMenu.add(pasteAction);
+	      
+	   
+	      //Submenu con Opciones
+	      negrita = new JButton ("N");
+	      negrita.setFont(letra1);
+	      negrita.setSize(60,50);
+	      
+	      //Cursiva
+	     cursiva = new JButton ("C");
+	     cursiva.setFont(letra2);
+	     cursiva.setSize(60,50);
+	     
+	     //Subrayado
+	     subr = new JButton ("N");
+	     subr.setFont(letra3);
+	     subr.setSize(60,50);
+	     
+	     //Color
+	     //JColorChooser dialogoColor = new JColorChooser(Color.BLUE);
+	     JButton color = new JButton ("Color");
+	     color.setSize(100,50);
+	     
+	      
+		//Barra Help
+		JMenu help = new JMenu ("Ayuda");
+		JMenuItem ABOUT = new JMenuItem ("Sobre Mash Text");
+		
+		//Barra de herramientas
+		JToolBar barra = new JToolBar();
+		
+		barra.setBounds(0, 25, 800, 30);
+		
+		barra.setFloatable(false); //impide que se pueda mover de sitio
+
+		barra.setOrientation(JToolBar.HORIZONTAL); // pone la orientación vertical
 
 		
 		//JPanel
@@ -67,15 +132,28 @@ public class Mash_Text  extends JFrame{
 		contenedor.add(principal);
 		principal.add(mostrar);
 		principal.add(menup);
+		principal.add(barra);
 		
 		//Aniadir menu barra
 		menup.add(files); 
+		menup.add(editMenu);
+		menup.add(help);
 		
 		//Aniadir menu 
 		files.add(nuevo);
 		files.add(guardar);
 		files.add(abrir);
 		files.add(salir);
+		
+		//Opciones ayuda
+		help.add(ABOUT);
+		
+		//Barra de heraamientas
+		barra.add(cursiva);
+		barra.add(negrita); 
+		barra.add(subr); 
+		barra.add(color);
+		
 		
 		//Eventos
 		//Salir
@@ -85,25 +163,6 @@ public class Mash_Text  extends JFrame{
 	        }
 	    });
 		
-		//Guardar
-		guardar.addActionListener(new ActionListener() {
-	        public void actionPerformed(ActionEvent ev) {
-	        	javax.swing.JFileChooser jF1= new javax.swing.JFileChooser();
-	            String ruta = "";
-	             try{
-	                if(jF1.showSaveDialog(null)==jF1.APPROVE_OPTION){
-	                  ruta = jF1.getSelectedFile().getAbsolutePath();
-	                  FileWriter archivo = new FileWriter (ruta);
-	                  archivo.write(mostrar.getText());
-	                  archivo.close();
-	                  
-	                //Aqui ya tiens la ruta,,,ahora puedes crear un fichero n esa ruta y escribir lo k kieras...
-	                }
-	              }catch (Exception ex){
-	                ex.printStackTrace();
-	              }
-	        }
-	    });
 		
 		//Abrir
 		abrir.addActionListener(new ActionListener() {
@@ -118,6 +177,69 @@ public class Mash_Text  extends JFrame{
 	        	mostrar.setText("");
 	        }
 	    });
+		
+		//AYUDA
+		ABOUT.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	 JOptionPane.showMessageDialog(getContentPane(), "Programa creado por Moisok  \n"
+	        	 		+ "Para la practica de programacion (DAM) de IES BENIGASLO");
+	        }
+	    });
+		
+		//Negrita
+		negrita.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	mostrar.setFont(letra1);
+	        }
+	    });
+		
+		//Cursiva
+		cursiva.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	mostrar.setFont(letra2);
+	        }
+	    });
+		
+		//Subrayado
+		subr.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	   Font font = mostrar.getFont();
+	               Map<TextAttribute, Object> attributes = new HashMap<>(font.getAttributes());
+	               attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+	               mostrar.setFont(font.deriveFont(attributes));
+	        }
+	    });
+		//Color
+		color.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ev) {
+	        	Color c = JColorChooser.showDialog(getContentPane(), "Selecciona el color", Color.BLUE);
+	        	mostrar.setForeground(c);
+	        	color.setBackground(c);
+	        	
+	        }
+	    });
+		
+		//Guardar
+				guardar.addActionListener(new ActionListener() {
+			        public void actionPerformed(ActionEvent ev) {
+			        	javax.swing.JFileChooser jF1= new javax.swing.JFileChooser();
+			            String ruta = "";
+			             try{
+			                if(jF1.showSaveDialog(null)==jF1.APPROVE_OPTION){
+			                  ruta = jF1.getSelectedFile().getAbsolutePath();
+			                  FileWriter archivo = new FileWriter (ruta);
+			                  archivo.write(mostrar.getText());
+			                  archivo.close();
+			                  
+			                //Aqui ya tiens la ruta,,,ahora puedes crear un fichero n esa ruta y escribir lo k kieras...
+			                }
+			              }catch (Exception ex){
+			                ex.printStackTrace();
+			              }
+			        }
+			    });
+		
+		
 	//Fin constructor	
 	}
 	
@@ -156,10 +278,16 @@ public class Mash_Text  extends JFrame{
 		  return texto;//El texto se almacena en el JTextArea
 		}
 
-
 		
-	
-	
+	//Funcion Fuentes
+	/*private void loadFont() {
+		GraphicsEnvironment gEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		// get all font name&amp;amp;amp;amp;nbsp;
+		String[] fontNames = gEnv.getAvailableFontFamilyNames();
+		// load to combobox
+		ComboBoxModel model = new DefaultComboBoxModel(fontNames);
+		jcbFont.setModel(model);
+		}*/
 	
 	//Clase principal
 	public static void main(String[] args) {
